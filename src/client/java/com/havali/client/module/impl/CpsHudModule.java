8d0be6fbeeabd22134d2720d785dd3d129dca3e0
atescinar544-alt <atescinar544@gmail.com>
2026-04-23 19:37:24 +0300

    Delete src directory

--- src/client/java/com/havali/client/module/impl/CpsHudModule.java
+++
@@ -1,6 +0,0 @@
-package com.havali.client.module.impl;
-import com.havali.client.module.Module;
-
-public class CpsHudModule extends Module {
-    public CpsHudModule() { super("CPS HUD"); }
-}
--- src/client/java/com/havali/client/module/impl/FpsHudModule.java
+++
@@ -1,6 +0,0 @@
-package com.havali.client.module.impl;
-import com.havali.client.module.Module;
-
-public class FpsHudModule extends Module {
-    public FpsHudModule() { super("FPS HUD"); }
-}
