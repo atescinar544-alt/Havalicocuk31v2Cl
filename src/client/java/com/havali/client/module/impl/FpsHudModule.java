1f10706879538ee5d6e2fd6d37297f5f2e1b9511
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 16:50:53 +0300

    Add FPS display to FpsHudModule
    Implemented onRender method to display FPS in HUD.

--- src/client/java/com/havali/client/module/impl/FpsHudModule.java
+++ src/client/java/com/havali/client/module/impl/FpsHudModule.java
@@ -1,5 +1,20 @@
 package com.havali.client.module.impl;
+
 import com.havali.client.module.Module;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+
 public class FpsHudModule extends Module {
-    public FpsHudModule() { super("FpsHud"); }
+    public FpsHudModule() {
+        super("FpsHud");
+    }
+
+    @Override
+    public void onRender(DrawContext context, float tickDelta) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.textRenderer != null) {
+            String fpsText = "FPS: " + client.getCurrentFps();
+            context.drawTextWithShadow(client.textRenderer, fpsText, 5, 5, 0x00FF00);
+        }
+    }
 }
