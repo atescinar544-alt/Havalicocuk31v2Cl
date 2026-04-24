b70b5e3e76645ffc6f540728d8978fcdcd218512
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 16:49:36 +0300

    Add onRender method to Module class

--- src/client/java/com/havali/client/module/Module.java
+++ src/client/java/com/havali/client/module/Module.java
@@ -1,22 +1,31 @@
 package com.havali.client.module;
+
+import net.minecraft.client.gui.DrawContext;
+
 public abstract class Module {
     private final String name;
     private boolean toggled;
+
     public Module(String name) {
         this.name = name;
         this.toggled = false;
     }
+
     public String getName() { return name; }
     public boolean isToggled() { return toggled; }
+
     public void toggle() {
         this.toggled = !this.toggled;
         if (this.toggled) onEnable();
         else onDisable();
     }
+
     public void setToggled(boolean toggled) {
         this.toggled = toggled;
     }
+
     public void onEnable() {}
     public void onDisable() {}
     public void onTick() {}
+    public void onRender(DrawContext context, float tickDelta) {}
 }
