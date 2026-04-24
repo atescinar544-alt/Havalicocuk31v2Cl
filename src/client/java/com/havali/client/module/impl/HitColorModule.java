cfb1767c514a01f4e96d40e36a1ec6f40d4e3667
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:23:08 +0300

    Implement RGB settings in HitColorModule
    Added RGB settings for HitColorModule.

--- src/client/java/com/havali/client/module/impl/HitColorModule.java
+++ src/client/java/com/havali/client/module/impl/HitColorModule.java
@@ -1,5 +1,15 @@
 package com.havali.client.module.impl;
+
 import com.havali.client.module.Module;
+import com.havali.client.module.settings.NumberSetting;
+
 public class HitColorModule extends Module {
-    public HitColorModule() { super("HitColor"); }
+    public static NumberSetting red = new NumberSetting("Red", 1.0, 0.0, 1.0, 0.1);
+    public static NumberSetting green = new NumberSetting("Green", 0.0, 0.0, 1.0, 0.1);
+    public static NumberSetting blue = new NumberSetting("Blue", 0.0, 0.0, 1.0, 0.1);
+
+    public HitColorModule() {
+        super("HitColor");
+        addSettings(red, green, blue);
+    }
 }
