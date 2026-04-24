e837135e5538a15d78040c4ff1314eb98ac9530f
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:17:15 +0300

    Create FogModule with color and density settings

--- src/client/java/com/havali/client/module/impl/ColorSaturationModule.java
+++
@@ -1,5 +0,0 @@
-package com.havali.client.module.impl;
-import com.havali.client.module.Module;
-public class ColorSaturationModule extends Module {
-    public ColorSaturationModule() { super("ColorSaturation"); }
-}
