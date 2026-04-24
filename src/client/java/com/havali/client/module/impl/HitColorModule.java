710615c48c002ee9842342674413189a8ecfa5ab
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 18:23:21 +0300

    Replace RGB settings with ColorSetting for HitColorModule

--- src/client/java/com/havali/client/module/impl/HitColorModule.java
+++ src/client/java/com/havali/client/module/impl/HitColorModule.java
@@ -1,15 +1,13 @@
 package com.havali.client.module.impl;
 
 import com.havali.client.module.Module;
-import com.havali.client.module.settings.NumberSetting;
+import com.havali.client.module.settings.ColorSetting;
 
 public class HitColorModule extends Module {
-    public static NumberSetting red = new NumberSetting("Red", 1.0, 0.0, 1.0, 0.1);
-    public static NumberSetting green = new NumberSetting("Green", 0.0, 0.0, 1.0, 0.1);
-    public static NumberSetting blue = new NumberSetting("Blue", 0.0, 0.0, 1.0, 0.1);
+    public static ColorSetting color = new ColorSetting("Hasar Rengi", 255, 0, 0);
 
     public HitColorModule() {
         super("HitColor");
-        addSettings(red, green, blue);
+        addSettings(color);
     }
 }
