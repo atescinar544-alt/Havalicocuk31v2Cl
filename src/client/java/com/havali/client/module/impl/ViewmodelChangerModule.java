77c420b274f688571d80b30c9793c341e5683fca
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:23:41 +0300

    Rename ViewmodelChangerModule to ViewmodelModule

--- src/client/java/com/havali/client/module/impl/ViewmodelChangerModule.java
+++ src/client/java/com/havali/client/module/impl/ViewmodelChangerModule.java
@@ -1,5 +1,16 @@
 package com.havali.client.module.impl;
+
 import com.havali.client.module.Module;
-public class ViewmodelChangerModule extends Module {
-    public ViewmodelChangerModule() { super("ViewmodelChanger"); }
+import com.havali.client.module.settings.NumberSetting;
+
+public class ViewmodelModule extends Module {
+    public static NumberSetting posX = new NumberSetting("PosX", 0.0, -2.0, 2.0, 0.1);
+    public static NumberSetting posY = new NumberSetting("PosY", 0.0, -2.0, 2.0, 0.1);
+    public static NumberSetting posZ = new NumberSetting("PosZ", 0.0, -2.0, 2.0, 0.1);
+    public static NumberSetting scale = new NumberSetting("Scale", 1.0, 0.1, 2.0, 0.1);
+
+    public ViewmodelModule() {
+        super("Viewmodel");
+        addSettings(posX, posY, posZ, scale);
+    }
 }
