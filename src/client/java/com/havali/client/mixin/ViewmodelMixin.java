6045f0bfe54d2c9b0574a8f9e550e99c8a995312
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:53:58 +0300

    Update ViewmodelMixin.java

--- src/client/java/com/havali/client/mixin/ViewmodelMixin.java
+++ src/client/java/com/havali/client/mixin/ViewmodelMixin.java
@@ -2,8 +2,11 @@ package com.havali.client.mixin;
 
 import com.havali.client.module.ModuleManager;
 import com.havali.client.module.impl.ViewmodelModule;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,12 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public class ViewmodelMixin {
-    @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
-    private void onRenderItem(float tickDelta, MatrixStack matrices, Object vertexConsumers, Object player, Hand hand, Object stack, CallbackInfo ci) {
+    @Inject(method = "renderItem", at = @At("HEAD"))
+    private void onRenderItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         ViewmodelModule mod = (ViewmodelModule) ModuleManager.INSTANCE.getModule("Viewmodel");
-        if (mod.toggled) {
+        if (mod != null && mod.toggled) {
             matrices.translate(ViewmodelModule.posX.getValue(), ViewmodelModule.posY.getValue(), ViewmodelModule.posZ.getValue());
-            matrices.scale(ViewmodelModule.scale.getValueFloat(), ViewmodelModule.scale.getValueFloat(), ViewmodelModule.scale.getValueFloat());
+            float s = ViewmodelModule.scale.getValueFloat();
+            matrices.scale(s, s, s);
         }
     }
 }
