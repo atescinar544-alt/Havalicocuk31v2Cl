f832fbbc393e8affc3633213af8fd78322e047c8
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:24:27 +0300

    Reorder and update modules in ModuleManager

--- src/client/java/com/havali/client/module/ModuleManager.java
+++ src/client/java/com/havali/client/module/ModuleManager.java
@@ -1,20 +1,28 @@
 package com.havali.client.module;
+
 import com.havali.client.module.impl.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class ModuleManager {
     public static final ModuleManager INSTANCE = new ModuleManager();
     private final List<Module> modules = new ArrayList<>();
+
     public ModuleManager() {
-        modules.add(new FpsHudModule());
-        modules.add(new CpsHudModule());
-        modules.add(new MotionBlurModule());
         modules.add(new ArmorHudModule());
-        modules.add(new ColorSaturationModule());
+        modules.add(new CpsHudModule());
+        modules.add(new FogModule());
         modules.add(new HitColorModule());
-        modules.add(new ViewmodelChangerModule());
+        modules.add(new ViewmodelModule());
+        modules.add(new FpsHudModule());
     }
-    public List<Module> getModules() {
-        return modules;
+
+    public List<Module> getModules() { return modules; }
+
+    public Module getModule(String name) {
+        for (Module m : modules) {
+            if (m.name.equalsIgnoreCase(name)) return m;
+        }
+        return null;
     }
 }
