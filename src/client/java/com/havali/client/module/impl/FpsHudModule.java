6f0c405b875526f0212495374c15a7b181f63509
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 17:41:31 +0300

    Implement background setting for FpsHudModule
    Added a background setting for the FPS HUD display.

--- src/client/java/com/havali/client/module/impl/FpsHudModule.java
+++ src/client/java/com/havali/client/module/impl/FpsHudModule.java
@@ -1,12 +1,16 @@
 package com.havali.client.module.impl;
 
 import com.havali.client.module.Module;
+import com.havali.client.module.settings.BooleanSetting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
 public class FpsHudModule extends Module {
+    public BooleanSetting background = new BooleanSetting("Background", true);
+
     public FpsHudModule() {
         super("FpsHud");
+        addSettings(background);
     }
 
     @Override
@@ -14,7 +18,10 @@ public class FpsHudModule extends Module {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.textRenderer != null) {
             String fpsText = "FPS: " + client.getCurrentFps();
-            context.drawTextWithShadow(client.textRenderer, fpsText, x, y, 0x00FF00);
+            if (background.isEnabled()) {
+                context.fill(x, y, x + 45, y + 15, 0x90000000);
+            }
+            context.drawTextWithShadow(client.textRenderer, fpsText, x + 5, y + 4, 0x00FF00);
         }
     }
 }
