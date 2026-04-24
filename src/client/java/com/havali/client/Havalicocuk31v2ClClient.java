c9f11cc6c65f169b455cfbf0f592c0ad8d4ad918
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 16:50:12 +0300

    Implement ClickGUI and module rendering functionality

--- src/client/java/com/havali/client/Havalicocuk31v2ClClient.java
+++ src/client/java/com/havali/client/Havalicocuk31v2ClClient.java
@@ -1,13 +1,19 @@
 package com.havali.client;
+
 import com.havali.client.gui.ClickGUI;
+import com.havali.client.module.Module;
+import com.havali.client.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+
 public class Havalicocuk31v2ClClient implements ClientModInitializer {
     private static KeyBinding clickGuiKey;
+
     @Override
     public void onInitializeClient() {
         clickGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
@@ -16,10 +22,19 @@ public class Havalicocuk31v2ClClient implements ClientModInitializer {
                 GLFW.GLFW_KEY_RIGHT_SHIFT,
                 "category.havaliclient.general"
         ));
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (clickGuiKey.wasPressed()) {
                 if (client.currentScreen == null) {
                     client.setScreen(new ClickGUI());
+                }
+            }
+        });
+
+        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
+            for (Module module : ModuleManager.INSTANCE.getModules()) {
+                if (module.isToggled()) {
+                    module.onRender(drawContext, tickDelta);
                 }
             }
         });
