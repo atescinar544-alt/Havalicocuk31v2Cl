cd1fadf51d35b1a401764d034d9838cc37682e18
atescinar544-alt <atescinar544@gmail.com>
2026-04-24 18:30:27 +0300

    Refactor FogModule settings to use ColorSetting

--- src/client/java/com/havali/client/module/impl/FogModule.java
+++ src/client/java/com/havali/client/module/impl/FogModule.java
@@ -1,16 +1,15 @@
 package com.havali.client.module.impl;
 
 import com.havali.client.module.Module;
+import com.havali.client.module.settings.ColorSetting;
 import com.havali.client.module.settings.NumberSetting;
 
 public class FogModule extends Module {
-    public static NumberSetting density = new NumberSetting("Density", 0.5, 0.1, 1.0, 0.05);
-    public static NumberSetting red = new NumberSetting("Red", 1.0, 0.0, 1.0, 0.01);
-    public static NumberSetting green = new NumberSetting("Green", 1.0, 0.0, 1.0, 0.01);
-    public static NumberSetting blue = new NumberSetting("Blue", 1.0, 0.0, 1.0, 0.01);
+    public static NumberSetting density = new NumberSetting("Yogunluk", 10.0, 0.0, 100.0);
+    public static ColorSetting color = new ColorSetting("Sis Rengi", 150, 0, 255);
 
     public FogModule() {
         super("Fog");
-        addSettings(density, red, green, blue);
+        addSettings(density, color);
     }
 }
