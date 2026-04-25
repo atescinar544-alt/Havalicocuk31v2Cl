f76386b682ebc447f7d2c5cc5f3bf5d3f6664ef7
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:26:24 +0300

    Format constructor for better readability

--- src/client/java/com/havali/client/module/settings/Setting.java
+++ src/client/java/com/havali/client/module/settings/Setting.java
@@ -2,5 +2,7 @@ package com.havali.client.module.settings;
 
 public class Setting {
     public String name;
-    public Setting(String name) { this.name = name; }
+    public Setting(String name) {
+        this.name = name;
+    }
 }
