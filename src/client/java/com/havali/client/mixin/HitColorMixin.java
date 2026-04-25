e912b021118ab099389bdd4b08e9e90480d0ad1c
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:56:09 +0300

    Enhance HitColorMixin to handle death overlay

--- src/client/java/com/havali/client/mixin/HitColorMixin.java
+++ src/client/java/com/havali/client/mixin/HitColorMixin.java
@@ -3,6 +3,7 @@ package com.havali.client.mixin;
 import com.havali.client.module.ModuleManager;
 import com.havali.client.module.impl.HitColorModule;
 import com.havali.client.module.settings.ColorSetting;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public class HitColorMixin {
@@ -18,7 +20,7 @@ public class HitColorMixin {
     @Inject(method = "render*", at = @At("HEAD"))
     private void onRenderHead(LivingEntity entity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         HitColorModule mod = (HitColorModule) ModuleManager.INSTANCE.getModule("HitColor");
-        if (mod != null && mod.toggled && entity.hurtTime > 0) {
+        if (mod != null && mod.toggled && (entity.hurtTime > 0 || entity.deathTime > 0)) {
             ColorSetting c = HitColorModule.color;
             int color = c.getColor();
             float r = ((color >> 16) & 0xFF) / 255.0f;
@@ -31,5 +33,13 @@ public class HitColorMixin {
     @Inject(method = "render*", at = @At("TAIL"))
     private void onRenderTail(LivingEntity entity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         com.mojang.blaze3d.systems.RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    @Inject(method = "getOverlay", at = @At("HEAD"), cancellable = true)
+    private static void onGetOverlay(LivingEntity entity, float whiteOverlayProgress, CallbackInfoReturnable<Integer> cir) {
+        HitColorModule mod = (HitColorModule) ModuleManager.INSTANCE.getModule("HitColor");
+        if (mod != null && mod.toggled && (entity.hurtTime > 0 || entity.deathTime > 0)) {
+            cir.setReturnValue(OverlayTexture.DEFAULT_UV);
+        }
     }
 }
