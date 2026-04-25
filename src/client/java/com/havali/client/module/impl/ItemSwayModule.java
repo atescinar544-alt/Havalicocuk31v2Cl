b82f9d40ed030b9980da1ae539633c30c0889f02
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 10:24:56 +0300

    Update and rename HitColorModule.java to ItemSwayModule.java

---
+++ src/client/java/com/havali/client/module/impl/ItemSwayModule.java
@@ -0,0 +1,10 @@
+package com.havali.client.module.impl;
+
+import com.havali.client.module.Module;
+
+public class ItemSwayModule extends Module {
+    public ItemSwayModule() {
+        super("ItemSway");
+        this.settings.clear();
+    }
+}
