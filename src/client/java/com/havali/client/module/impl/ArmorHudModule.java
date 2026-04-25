36c2cf5758d51462e711a565727235c72aee1158
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:31:56 +0300

    Refactor ArmorHudModule rendering logic and position

--- src/client/java/com/havali/client/module/impl/ArmorHudModule.java
+++ src/client/java/com/havali/client/module/impl/ArmorHudModule.java
@@ -4,28 +4,41 @@ import com.havali.client.module.Module;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.collection.DefaultedList;
 
 public class ArmorHudModule extends Module {
+
     public ArmorHudModule() {
         super("ArmorHud");
-        this.x = 300;
-        this.y = 200;
+        this.x = 20;
+        this.y = 100;
     }
 
     @Override
     public void onRender(DrawContext context, float tickDelta) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player == null) return;
-        int renderY = this.y;
-        DefaultedList<ItemStack> armor = client.player.getInventory().armor;
-        for (int i = 3; i >= 0; i--) {
-            ItemStack stack = armor.get(i);
-            if (!stack.isEmpty()) {
-                context.drawItem(stack, this.x, renderY);
-                context.drawItemInSlot(client.textRenderer, stack, this.x, renderY);
-                renderY += 18;
-            }
+
+        context.getMatrices().push();
+        context.getMatrices().translate(x, y, 0);
+        context.getMatrices().scale(scale.getValueFloat(), scale.getValueFloat(), 1.0f);
+
+        int currentY = 82;
+        ItemStack mainHand = client.player.getInventory().getMainHandStack();
+        renderItem(context, mainHand, 0, currentY);
+
+        currentY -= 20;
+        for (int i = 0; i <= 3; i++) {
+            ItemStack item = client.player.getInventory().armor.get(i);
+            renderItem(context, item, 0, currentY);
+            currentY -= 20;
         }
+        context.getMatrices().pop();
+    }
+
+    private void renderItem(DrawContext context, ItemStack stack, int localX, int localY) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        context.drawItem(stack, localX, localY);
+        String countText = stack.getCount() > 1 ? String.valueOf(stack.getCount()) : null;
+        context.drawItemInSlot(client.textRenderer, stack, localX, localY, countText);
     }
 }
