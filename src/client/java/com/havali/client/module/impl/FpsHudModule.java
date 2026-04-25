77479a753bb7617423d33c3862bc4ff3c123e822
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:30:57 +0300

    Update FpsHudModule to use textColor setting

--- src/client/java/com/havali/client/module/impl/FpsHudModule.java
+++ src/client/java/com/havali/client/module/impl/FpsHudModule.java
@@ -8,23 +8,30 @@ import net.minecraft.client.gui.DrawContext;
 
 public class FpsHudModule extends Module {
     public BooleanSetting background = new BooleanSetting("Arkaplan", true);
-    public ColorSetting color = new ColorSetting("Tema Rengi", 0, 255, 204);
+    public ColorSetting textColor = new ColorSetting("Yazi Rengi", 0.0f, 0.0f, 1.0f);
 
     public FpsHudModule() {
         super("FpsHud");
-        addSettings(background, color);
+        addSettings(background, textColor);
     }
 
     @Override
     public void onRender(DrawContext context, float tickDelta) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.textRenderer != null) {
-            String text = "FPS: " + client.getCurrentFps();
+            String text = client.getCurrentFps() + " FPS";
+            
+            context.getMatrices().push();
+            context.getMatrices().translate(x, y, 0);
+            context.getMatrices().scale(scale.getValueFloat(), scale.getValueFloat(), 1.0f);
+            
             if (background.isEnabled()) {
-                context.fill(x, y, x + 60, y + 16, 0x90101010); 
-                context.fill(x, y, x + 2, y + 16, color.getColor());
+                int width = client.textRenderer.getWidth(text) + 8;
+                context.fill(0, 0, width, 14, 0x90000000);
             }
-            context.drawTextWithShadow(client.textRenderer, text, x + 6, y + 4, 0xFFFFFF);
+            
+            context.drawTextWithShadow(client.textRenderer, text, 4, 3, textColor.getColor());
+            context.getMatrices().pop();
         }
     }
 }
