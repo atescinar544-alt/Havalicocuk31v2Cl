3b0c8c8cb965ae5c6f35d109ee408e39029c7cc7
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:26:49 +0300

    Refactor isEnabled and setEnabled methods

--- src/client/java/com/havali/client/module/settings/BooleanSetting.java
+++ src/client/java/com/havali/client/module/settings/BooleanSetting.java
@@ -2,10 +2,17 @@ package com.havali.client.module.settings;
 
 public class BooleanSetting extends Setting {
     public boolean enabled;
+    
     public BooleanSetting(String name, boolean enabled) {
         super(name);
         this.enabled = enabled;
     }
-    public boolean isEnabled() { return enabled; }
-    public void setEnabled(boolean enabled) { this.enabled = enabled; }
+    
+    public boolean isEnabled() {
+        return enabled;
+    }
+    
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
