f83180a701f9e59a7ff306f7042aa5ab8c15c3ab
atescinar544-alt <atescinar544@gmail.com>
2026-04-25 09:30:20 +0300

    Enhance drag functionality for HUD modules

--- src/client/java/com/havali/client/gui/HudEditorScreen.java
+++ src/client/java/com/havali/client/gui/HudEditorScreen.java
@@ -2,24 +2,40 @@ package com.havali.client.gui;
 
 import com.havali.client.module.Module;
 import com.havali.client.module.ModuleManager;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
 public class HudEditorScreen extends Screen {
     private Module dragging = null;
-    public HudEditorScreen() { super(Text.literal("HUD Editor")); }
+    private int dragOffsetX = 0;
+    private int dragOffsetY = 0;
+
+    public HudEditorScreen() {
+        super(Text.literal("HUD Editor"));
+    }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context);
+        
         for (Module mod : ModuleManager.INSTANCE.getModules()) {
             if (mod.toggled) {
-                context.fill(mod.x, mod.y, mod.x + 50, mod.y + 20, 0x70FFFFFF);
-                context.drawTextWithShadow(textRenderer, mod.name, mod.x + 2, mod.y + 5, 0xFFFFFF);
+                float s = mod.scale.getValueFloat();
+                int boxW = (int) (60 * s);
+                int boxH = (int) (20 * s);
+                
+                context.fill(mod.x, mod.y, mod.x + boxW, mod.y + boxH, 0x40FFFFFF);
+                
+                context.getMatrices().push();
+                context.getMatrices().translate(mod.x, mod.y, 0);
+                context.getMatrices().scale(s, s, 1.0f);
+                context.drawTextWithShadow(textRenderer, mod.name, 2, 6, 0xFFFFFF);
+                context.getMatrices().pop();
+
                 if (dragging == mod) {
-                    mod.x = mouseX - 25;
-                    mod.y = mouseY - 10;
+                    mod.x = mouseX - dragOffsetX;
+                    mod.y = mouseY - dragOffsetY;
                 }
             }
         }
@@ -29,9 +45,17 @@ public class HudEditorScreen extends Screen {
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         for (Module mod : ModuleManager.INSTANCE.getModules()) {
-            if (mouseX >= mod.x && mouseX <= mod.x + 50 && mouseY >= mod.y && mouseY <= mod.y + 20) {
-                dragging = mod;
-                return true;
+            if (mod.toggled) {
+                float s = mod.scale.getValueFloat();
+                int boxW = (int) (60 * s);
+                int boxH = (int) (20 * s);
+                
+                if (mouseX >= mod.x && mouseX <= mod.x + boxW && mouseY >= mod.y && mouseY <= mod.y + boxH) {
+                    dragging = mod;
+                    dragOffsetX = (int) mouseX - mod.x;
+                    dragOffsetY = (int) mouseY - mod.y;
+                    return true;
+                }
             }
         }
         return super.mouseClicked(mouseX, mouseY, button);
